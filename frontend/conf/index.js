65039f60bf4db3b79faf7b2b664c89dd87c93239
root <root@Einstein.localdomain>
2022-12-26 20:40:05 +0530

    changed backend endpoint to onrender

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "https://hasansam-qtrip-dynamic.herokuapp.com" };
+const config = { backendEndpoint: "https://qtrip-dynamic-zg08.onrender.com" };
 
 export default config;
